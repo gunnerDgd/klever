4fa4ded4a0db912ad24f6f71a8de4285e1c396e4
gunnerDgd <ifstudiomail@naver.com>
2024-06-16 17:23:42 +0900

    [Fix] Busybox -> make menuconfig error because of busybox makefile error (Check Failed because of -Wimplicit-int)

--- linux/tools/busybox.cmake
+++ linux/tools/busybox.cmake
@@ -1,10 +1,27 @@
 function   (enable_busybox VERSION)
+    string (APPEND PATH    ${KLEVER_PATH_TOOLS}/busybox)
     string (APPEND NAME    "busybox-${VERSION}")
     string (APPEND ARCHIVE "${NAME}.tar.bz2")
     string (APPEND URL     "https://busybox.net/downloads/${ARCHIVE}")
 
+    if   (EXISTS /usr/bin/konsole)
+        string (APPEND CONF "konsole --workdir ${PATH} --hold -e ")
+        string (APPEND CONF "make menuconfig HOSTCC=")
+        string (APPEND CONF "\"gcc -Wno-implicit-int\" ")
+        string (APPEND CONF "-j${KLEVER_JOB}")
+    endif()
+
     if   (EXISTS ${KLEVER_PATH_TOOLS}/busybox)
-        set   (KLEVER_TOOLS_BUSYBOX ON)
+        if   (NOT EXISTS ${KLEVER_PATH_TOOLS}/busybox/.config)
+            execute_process(COMMAND /bin/bash    -c ${CONF}       WORKING_DIRECTORY ${PATH})
+            execute_process(COMMAND make busybox -j ${KLEVER_JOB} WORKING_DIRECTORY ${PATH})
+        endif()
+
+        if    (NOT EXISTS ${KLEVER_PATH_TOOLS}/busybox/busybox)
+            return()
+        endif ()
+
+        set   (KLEVER_TOOLS_BUSYBOX ON PARENT_SCOPE)
         return()
     endif()
 
@@ -21,15 +38,8 @@ function   (enable_busybox VERSION)
 
     file  (RENAME ${KLEVER_PATH_TOOLS}/${NAME} ${KLEVER_PATH_TOOLS}/busybox)
     file  (REMOVE ${KLEVER_PATH_TOOLS}/${ARCHIVE})
-    string(APPEND PATH ${KLEVER_PATH_TOOLS}/busybox)
 
-    if   (EXISTS /usr/bin/konsole)
-        string (APPEND CONF "konsole --workdir ${PATH} --hold -e ")
-        string (APPEND CONF "make menuconfig -j${KLEVER_JOB}")
-    endif()
-
-    execute_process(COMMAND make defconfig       WORKING_DIRECTORY ${PATH})
-    execute_process(COMMAND /bin/bash -c ${CONF} WORKING_DIRECTORY ${PATH})
-    execute_process(COMMAND make busybox         WORKING_DIRECTORY ${PATH})
-    set            (KLEVER_TOOLS_BUSYBOX ON)
+    execute_process(COMMAND /bin/bash    -c ${CONF}       WORKING_DIRECTORY ${PATH})
+    execute_process(COMMAND make busybox -j ${KLEVER_JOB} WORKING_DIRECTORY ${PATH})
+    set            (KLEVER_TOOLS_BUSYBOX ON PARENT_SCOPE)
 endfunction()
