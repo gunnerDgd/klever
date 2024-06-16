e6df4720a5386f9451c52ae522e1c48e78835e61
gunnerDgd <ifstudiomail@naver.com>
2024-06-16 17:24:08 +0900

    [Fix] make clean Command Not Executed

--- linux/kernel/clean.cmake
+++ linux/kernel/clean.cmake
@@ -1,11 +1,9 @@
-function   (kernel_clean NAME)
-    string (APPEND PATH    "${KLEVER_PATH_KERNEL}/${NAME}")
-    string (APPEND COMMAND "make clean")
-
-    if   (NOT EXISTS ${PATH})
+function  (kernel_clean NAME)
+    string(APPEND PATH    "${KLEVER_PATH_KERNEL}/${NAME}")
+    if    (NOT EXISTS ${PATH})
         message("[Klever] Kernel named ${NAME} not found at ${KLEVER_PATH_KERNEL}")
         message(SEND_ERROR "Abort")
     endif()
 
-    add_custom_target(${NAME}-clean COMMAND ${COMMAND} WORKING_DIRECTORY ${PATH})
+    add_custom_target(${NAME}-clean COMMAND sudo -S make clean WORKING_DIRECTORY ${PATH})
 endfunction()
