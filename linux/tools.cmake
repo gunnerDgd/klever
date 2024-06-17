61cdec3ce9d8a916e4f6b9ad0caf1c33c80e5f9d
gunnerDgd <ifstudiomail@naver.com>
2024-06-17 17:03:29 +0900

    [Fix] KLEVER_PATH_TOOLS missing on scope

--- linux/tools.cmake
+++ linux/tools.cmake
@@ -1,4 +1,5 @@
 set (KLEVER_PATH_TOOLS ${KLEVER_PATH}/tools PARENT_SCOPE)
+set (KLEVER_PATH_TOOLS ${KLEVER_PATH}/tools)
 file(MAKE_DIRECTORY    ${KLEVER_PATH}/tools)
 
 include (${CMAKE_CURRENT_LIST_DIR}/tools/busybox.cmake)
